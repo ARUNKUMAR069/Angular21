be655ac5d1c133d12e874e081acebb41d3e99b1a
Arun_Kumar <stockerunknown@gmail.com>
2026-01-11 22:25:35 +0530

    learn+implement : Dynamic Styling in the Angular

---
+++ 23-Dynamic-Styling/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('23-Dynamic-Styling');
+
+  color='red';
+  fontSize='100px';
+}
